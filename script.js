0327edd2ca30b3b2797ca57bc7a5f692e3481e49
mariaangela <136354649+angelalala01@users.noreply.github.com>
2024-06-11 10:15:22 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('button').addEventListener('click', function() {
+    document.querySelector('.mainContentSection').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
